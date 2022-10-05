dc1ae8b9976b769233d767cd8e87449b6719b955
Hirotada Takeuchi <t.hir0tada.biz@gmail.com>
2022-10-05 15:44:15 +0900

    union aliases

--- app.js
+++ app.js
@@ -1,20 +1,19 @@
-// const person :{
-//   name:string;
-//   age:number;
-// } = {
-var person = {
-    name: 'AAA',
-    age: 30,
-    hobbies: ['sports', 'cooking']
-};
-var activities;
-activities = ['sports'];
-/* (error)
-activities = 'sports'
-activities = ['sports',1]
- */
-console.log(person.name);
-for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
-    var hobby = _a[_i];
-    console.log(hobby);
+function conbine(input1, input2, resultConversion) {
+    var result;
+    if ((typeof input1 === "number" && typeof input2 === "number") ||
+        resultConversion === "as-number") {
+        result = +input1 + +input2;
+    }
+    else {
+        result = input1.toString() + input2.toString();
+    }
+    return result;
+    // if (resultConversion === 'as-number') return +result;
+    // return result.toString();
 }
+var conbinedAges = conbine(30, 26, "as-number");
+console.log(conbinedAges);
+var conbinedStringAges = conbine("30", "26", "as-number");
+console.log(conbinedStringAges);
+var conbinedNames = conbine("Max", "Anna", "as-text");
+console.log(conbinedNames);
--- app.ts
+++ app.ts
@@ -1,28 +1,30 @@
-// const person :{
-//   name:string;
-//   age:number;
-// } = {
-const person :{
-  name:string;
-  age:number;
-  hobbies:string[];
-  role:[number, string];
-} = {
-  name:'AAA',
-  age:30,
-  hobbies:['sports', 'cooking'],
-  role:[2,'author']
-};
+type Combinable = number | string;
+type ConversionDescriptor = 'as-number' | 'as-text';
 
-let activities:string[];
-activities = ['sports'];
-/* (error)
-activities = 'sports'
-activities = ['sports',1]
- */
-
-console.log(person.name);
-
-for (const hobby of person.hobbies) {
-  console.log(hobby.toUpperCase());
+function conbine(
+  input1: Combinable,
+  input2: Combinable,
+  resultConversion: ConversionDescriptor,
+) {
+  let result;
+  if (
+    (typeof input1 === "number" && typeof input2 === "number") ||
+    resultConversion === "as-number"
+  ) {
+    result = +input1 + +input2;
+  } else {
+    result = input1.toString() + input2.toString();
+  }
+  return result;
+  // if (resultConversion === 'as-number') return +result;
+  // return result.toString();
 }
+
+const conbinedAges = conbine(30, 26, "as-number");
+console.log(conbinedAges);
+
+const conbinedStringAges = conbine("30", "26", "as-number");
+console.log(conbinedStringAges);
+
+const conbinedNames = conbine("Max", "Anna", "as-text");
+console.log(conbinedNames);
