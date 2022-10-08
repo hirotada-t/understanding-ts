0c01941cd93335518e93fd0f675df11fd260ccf1
Hirotada Takeuchi <t.hir0tada.biz@gmail.com>
2022-10-08 15:35:38 +0900

    config

--- app.js
+++ app.js
@@ -1,15 +1,5 @@
-var userInput;
-var userName;
-userInput = 4;
-userInput = "sss";
-if (typeof userInput === "string")
-    userName = userInput;
-function generateError(message, code) {
-    throw { message: message, errorCode: code };
-}
-try {
-    generateError("エラーが発生しました", 500);
-}
-catch (e) {
-    console.log(e);
-}
+"use strict";
+const btn = document.querySelector("button");
+btn.addEventListener("click", () => {
+    alert("clicked !!");
+});
